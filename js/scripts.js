a8dd47a92f3ad302b3c92bd66aeb1dfe4cc63ddb
Trupti7291 <87382991+Trupti7291@users.noreply.github.com>
2021-07-26 17:44:11 +0200

    Update scripts.js
    added content in the file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-alert('Hello world');
+//alert('Hello world');
+console.log('Hello world');
+
+let age = 30;
+let name = 'Trupti';
+
+let myAge = 35;
+let lisasAge = myAge;
+
+let myName = 'Trupti.';
+document.write(myName);
+myName = 'Trupti Hirani';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+/*let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+*/
