970626331cc9f02af14a1697a029d110a9a52ed5
Trupti7291 <87382991+Trupti7291@users.noreply.github.com>
2021-07-30 10:48:57 +0200

    Create scripts.js
    added PokemonList

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let pokemonList = [
+    { name: 'Braviary', height: 5, type: ['normal', 'flying'] },
+    { name: 'Ho-oh', height: 4, type: ['flying', 'fairy', 'fire', 'grass'] },
+    { name: 'Piplup', height: 1, type: ['water', 'ice'] },
+    { name: 'Rapidash', height: 1.7, type: ['fire', 'ice'] }
+
+];
